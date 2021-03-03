ad5252563cb606d5ffcb46b581f2864c7192e3fe
Anmolkumar Shah <anmolshah.20k@gmail.com>
2021-03-03 17:33:22 +0530

    support for sqreen size

--- src/App.js
+++ src/App.js
@@ -4,30 +4,38 @@ import { AFrameRenderer, Marker } from "react-web-ar";
 class App extends Component {
   render() {
     return (
-      <AFrameRenderer
-        arToolKit={{
-          sourceType: "webcam",
-          sourceUrl: "./images/hiro_marker.png",
-        }}
-        stats
-      >
-        <Marker parameters={{ preset: "hiro" }}>
-          <a-box
-            color="pink"
-            material="opacity: 1;"
-            position="0 0.003 0"
-            scale="0.4 0.4 0.4"
-          >
-            <a-animation
-              attribute="rotation"
-              to="360 0 0"
-              dur="5000"
-              easing="linear"
-              repeat="indefinite"
-            />
-          </a-box>
-        </Marker>
-      </AFrameRenderer>
+      <div style={{ height: "100%", width: "100%", overflow: "hidden" }}>
+        <AFrameRenderer
+          inherent={true}
+          arToolKit={{
+            sourceType: "webcam",
+            sourceHeight: 1,
+            sourceWidth: 1,
+            displayHeight: 1,
+            displayWidth: 1,
+            debugUIEnabled: "true",
+            canvasHeight: 1,
+            canvasWidth: 1,
+          }}
+        >
+          <Marker parameters={{ preset: "hiro" }}>
+            <a-box
+              color="pink"
+              material="opacity: 1;"
+              position="0 0.003 0"
+              scale="0.4 0.4 0.4"
+            >
+              <a-animation
+                attribute="rotation"
+                to="360 0 0"
+                dur="5000"
+                easing="linear"
+                repeat="indefinite"
+              />
+            </a-box>
+          </Marker>
+        </AFrameRenderer>
+      </div>
     );
   }
 }
